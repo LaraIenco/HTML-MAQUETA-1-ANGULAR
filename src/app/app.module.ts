24dc7b4c0cee95fe29243d18e542ccdc29f85f82
LaraIenco <marce6810@hotmail.com>
2022-10-12 20:47:35 -0300

    ultimo update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoapComponent } from './components/logoap/logoap.component';
+import { SocialComponent } from './components/social/social.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoapComponent,
+    SocialComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule
